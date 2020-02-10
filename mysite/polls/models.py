4bfbf7d17d76ad1816b5308405c58ce0e7766aa8
JBU-seol <seolki100480@gmail.com>
2020-02-10 22:10:41 +0900

    tutorial #2

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,3 +1,21 @@
+import datetime
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published') #날짜와 시간 필드
+    def __str__(self):
+        return self.question_text
+
+    def was_published_recently(self):#pub_date시간이 현재시각 기준 하루가 안지났는지.
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    # 각각의 Choice 가 하나의 Question 에 관계된다.
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+    def __str__(self):
+        return self.choice_text
+
